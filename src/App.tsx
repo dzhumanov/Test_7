ed6f1f5527330c71154a5c9cbc42eb0ee8016524
Temirlan Dzhumanov <temadjumanov@gmail.com>
2023-11-04 12:50:45 +0600

    Downloaded images, made constant and initialized state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [orders, setOrders] = useState([]);
+  
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h4>Order details</h4>
+          <div className="orders"></div>
+        </div>
+        <div className="col">
+          <h4>Add items:</h4>
+          <div className="row orderList">
+            <div className="col-sm-6 mb-3 mb-sm-0">
+              <div className="card d-flex flex-row align-items-center ps-1">
+                <div className="img">
+                  <img
+                    src="src/assets/burger.png"
+                    className="card-pic"
+                    alt=""
+                  />
+                </div>
+                <div className="card-body flex-grow-1">
+                  <h5 className="card-title">Food name</h5>
+                  <p className="card-text">price: 100kgs</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Constants/Constant.ts
@@ -0,0 +1,21 @@
+import burgerImage from "../assets/burger.png";
+import pizzaImage from "../assets/pizza.png";
+import hotdogImage from "../assets/hotdog.png";
+import corndogImage from "../assets/corndog.png";
+import friesImage from "../assets/fries.png";
+import tacoImage from "../assets/taco.png";
+import colaImage from "../assets/cola.png";
+import sodaImage from "../assets/soda.png";
+
+const FOOD = [
+  { name: "Burger", cost: 100, img: burgerImage },
+  { name: "Pizza", cost: 100, img: pizzaImage },
+  { name: "Hot Dog", cost: 100, img: hotdogImage },
+  { name: "Corn Dog", cost: 100, img: corndogImage },
+  { name: "Fries", cost: 100, img: friesImage },
+  { name: "Taco", cost: 100, img: tacoImage },
+  { name: "Cola", cost: 100, img: colaImage },
+  { name: "Soda", cost: 100, img: sodaImage },
+];
+
+export default FOOD;
